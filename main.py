35939522d24d265d1f664bef1bdc3049ece684b8
Felipe Sihel <felipesihel@gmail.com>
2025-02-19 20:20:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+value = 1
+count = 1
+
+print(f"you have {value} dolah. \nwhat you want: \n(D)ouble it \n(G)et current")
+
+M = input("What you want? D or G\n")
+print(M)
+
+while count < 32:
+    if M == "D":
+        value *= 2
+        count += 1
+        print(f"you now have {value} dolah \n level {count}")
+        M = input("What you want? D or G\n")
+        if count > 31:
+            print("OVERFLOW HAS ATTACKED\n GAME OVER\n *ENDING 2*")
+            break
+
+    elif M == "G":
+        print(f"you got {value} dolah\n GAME OVER\n *ENDING 1*")
+        break
+
+    else:
+        print("Input D or G")
+        M = input("What you want? D or G\n")
