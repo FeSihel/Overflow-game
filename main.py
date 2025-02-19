aa59c978481deb99bc356aa090eac5a0ca7c92c1
Felipe Sihel <felipesihel@gmail.com>
2025-02-19 20:54:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ count = 1
 print(f"you have {value} dolah. \nwhat you want: \n(D)ouble it \n(G)et current")
 
 M = input("What you want? D or G\n")
-print(M)
 
 while count < 32:
     if M == "D":
@@ -13,7 +12,9 @@ while count < 32:
         print(f"you now have {value} dolah \n level {count}")
         M = input("What you want? D or G\n")
         if count > 31:
+            value *= -1
             print("OVERFLOW HAS ATTACKED\n GAME OVER\n *ENDING 2*")
+            print(f"you got {value} dolah")
             break
 
     elif M == "G":
